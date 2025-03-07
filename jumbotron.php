2c8c435ef802e4a70f9f65e991d71e14dad50b0a
kristina875 <ksanchez23-0337@cca.edu.ph>
2025-03-07 19:01:05 +0800

    werwer
    rdef

---
+++ jumbotron.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="mt-4 p-5 bg-primary text-white rounded">
+  <h1>Jumbotron Example</h1>
+  <p>the dog jump over the sleeping fox</p>
+</div>                                       
+</body>
+</html>
